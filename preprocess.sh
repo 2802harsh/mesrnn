94c02ba7b15b66dc8c299cf444303b2fe6e0b740
Ranveer Singh <sidana.manik251@gmail.com>
2023-06-20 22:51:51 +0530

    Update preprocess.sh

--- preprocess.sh
+++ preprocess.sh
@@ -5,5 +5,5 @@ datasets=("eth_hotel" "eth_univ")
 # datasets=("biwi_hotel" "crowds_students003" "crowds_students001" "crowds_zara01" "crowds_zara03" "lcas" "wildtrack")
 
 for dataset in ${datasets[@]}; do
-  python3 ./data/preprocessor.py --dataset $dataset --data_path ./data/trajnet_dataset --skip 10 --frame_stride 2
+  python3 ./data/preprocessor.py --dataset $dataset --data_path ./data --skip 10 --frame_stride 2
 done
